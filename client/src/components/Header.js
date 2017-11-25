5046cbe2ad1af8c95c0449da74025eea127d4e1a
DanielRSnell <hi@danielsnell.ninja>
2017-11-25 16:47:20 -0500

    Typo HeadersJS Auth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,17 +4,25 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Header extends Component {
-  renderContent() {
-    switch (this.props.auth) {
-      case null:
-        return;
-      case false:
-        return <li><a href="/auth/google">LOG IN</a></li>;
-      default:
-        return [
-          <li key="2"><a href="/api/logout">LOGOUT</a></li>
-        ];
-    }
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href="/auth/google">LOG IN</a>
+					</li>
+				);
+			default:
+				return [
+					<li key="2">
+						<a href="/api/logout">LOGOUT</a>
+					</li>
+				];
+		}
+	}
+
 	render() {
 		return (
 			<nav>
