87102f7afb893fcc4abafc7cc7bc2459acdea45d
DanielRSnell <hi@danielsnell.ninja>
2017-11-25 15:40:53 -0500

    client side production added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/users');
 require('./services/passport');
@@ -12,6 +13,9 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 // Establish cookies within the application
+
+app.use(bodyParser.json());
+
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -23,7 +27,18 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes.js')(app);
+require('./routes/authRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	//This will serve production React Enviroment
+
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 // Setting up heroku - Dynamic Binding
 const PORT = process.env.PORT || 5000;
